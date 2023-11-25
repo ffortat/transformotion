3f9b9c7db0aff2399e8dc973880e2e5237b47c02
Plobi <mrplobi@gmail.com>
2023-11-25 22:50:06 +0100

    Fixed lock when no conditional placement

--- Assets/Scripts/ScriptableScripts/Keyword.cs
+++ Assets/Scripts/ScriptableScripts/Keyword.cs
@@ -9,12 +9,14 @@ public class Keyword : ScriptableObject
     public List<KeywordReaction> keywordReaction;
     public KeywordReaction defaultReaction;
 
-    public bool IsValidInContext(KeywordContext keywordContext)
+    public bool IsValidInContext(KeywordContext contextToMatch)
     {
+        if (keywordReaction.Count == 0) return true; //No need to check conditions when no conditional reaction
+
         bool isValid = false;
         foreach (KeywordReaction reaction in keywordReaction)
         {
-            if(reaction.context.currentLocation == null || reaction.context.Equals(keywordContext))
+            if(reaction.context.currentLocation == null || reaction.context.Equals(contextToMatch))
             {
                 isValid = true;
                 break;
